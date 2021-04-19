cafb09e562b227605e8c86b27f26fdc26d5bd1a9
GI-10836535 <82456010+GI-10836535@users.noreply.github.com>
2021-04-19 18:59:01 +0000

    Add files via upload

---
+++ login-user.php
@@ -0,0 +1,66 @@
+<?php require_once "controllerUserData.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 form login-form">
+                <form action="login-user.php" method="POST" autocomplete="">
+                    <h2 class="text-center"><b>Login Form</b></h2><br>
+                    <p class="text-center">Login with your email and password.</p><br>
+                    <?php
+                    if(count($errors) > 0){
+                        ?>
+                        <div class="alert alert-danger text-center">
+                            <?php
+                            foreach($errors as $showerror){
+                                echo $showerror;
+                            }
+                            ?>
+                        </div>
+                        <?php
+                    }
+                    ?>
+                    <div class="form-group">
+                        <input class="form-control" type="email" name="email" placeholder="Email Address" required value="<?php echo $email ?>">
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" type="password" name="password" placeholder="Password" required>
+                    </div>
+                    <div class="link forget-pass text-left"><a href="forgot-password.php">Forgot password?</a></div>
+                    <div class="remember">
+            <input type="checkbox" id="remeber" name="remember" value="Remember Me">
+            <label for = "remember">Remember me</label>
+        </div>
+                    <div class="form-group">
+                        <input class="form-control button" type="submit" name="login" value="Login">
+                    </div>
+                    <div class="link login-link text-center">Not yet a member? <a href="signup-user.php">Signup</a></div>
+                </form>
+            </div>
+        </div>
+        <script>
+      const pass_field = document.querySelector('.pass-key');
+      const showBtn = document.querySelector('.show');
+      showBtn.addEventListener('click', function(){
+       if(pass_field.type === "password"){
+         pass_field.type = "text";
+         showBtn.textContent = "HIDE";
+         showBtn.style.color = "#3498db";
+       }else{
+         pass_field.type = "password";
+         showBtn.textContent = "SHOW";
+         showBtn.style.color = "#222";
+       }
+      });
+    </script>
+    </div>
+    
+</body>
+</html>
